7e898d74f590d2b3b06f981105e74b016c00ee50
diogofbrito <diogobrito74@gmail.com>
2025-10-08 17:56:40 +0100

    feat: new

--- src/pages/IPlusD.jsx
+++ src/pages/IPlusD.jsx
@@ -30,7 +30,7 @@ export function IPlusD() {
 
 	return (
 		<div className='w-full px-12 pb-12'>
-			<div className='grid grid-cols-3 gap-[100px]'>
+			<div className='grid grid-cols-4 gap-[100px]'>
 				{imaisd.map(item => (
 					<div key={item._id} className='relative group overflow-hidden'>
 						<Link to={`/imaisd/${item.slug}`} className='block relative'>
@@ -38,13 +38,13 @@ export function IPlusD() {
 								<img
 									src={urlFor(item.placeholderImage).width(1000).quality(80).auto('format').url()}
 									alt={item.title}
-									className='w-full h-[300px] object-cover transition-all duration-700 ease-out group-hover:opacity-20 group-hover:scale-[1.05]'
+									className='w-full h-[400px] object-cover transition-all duration-700 ease-out group-hover:opacity-20 group-hover:scale-[1.05]'
 								/>
 							)}
 
 							<div className='absolute inset-0 flex flex-col items-center justify-center text-center text-black opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out'>
 								<AnimatedH1 className='font-[500] uppercase tracking-[0.03em] text-lg'>{item.title}</AnimatedH1>
-								<AnimatedPAfterH1 className='font-[400] text-base mt-1'>{item.year}</AnimatedPAfterH1>
+								
 							</div>
 						</Link>
 					</div>
